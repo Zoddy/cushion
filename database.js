f18c801e7603554f1b6523b021b3e3f2c26d8918
André Kussmann <zoddy@meecrom.de>
2012-01-22 09:32:24 +0100

    add function to check if a database exists or not

--- database.js
+++ database.js
@@ -5,6 +5,26 @@ var database = function(name, connection) {
 
 
 /**
+ * checks if a database exists
+ *
+ * @param {function(error, exists)} callback function that will be called,
+ *     after we had the information if the database exists or not
+ */
+database.prototype.exists = function(callback) {
+  this.getInfo(function(error, response) {
+    if (response !== null) {
+      if (response.error === 'not_found') {
+        response = false;
+      } else {
+        response = true;
+      }
+    }
+
+    callback(error, response);
+  });
+};
+
+/**
  * gets info about a database
  *
  * @param {string} name name of the database
