6ec71843b4a94aa5be9ae89cee5c698e8b31c672
André Kussmann <zoddy@meecrom.de>
2012-01-23 22:57:10 +0100

    add function to create document

--- document.js
+++ document.js
@@ -14,4 +14,28 @@ var Document = function(id, revision, connection, database) {
 };
 
 
+/**
+ * create the document
+ *
+ * @param {Object} body content of the document
+ * @param {function(error, nodecouch.Document)} callback function that will be
+ *     called, after document was created, or if there was an error
+ */
+Document.prototype.create = function(body, callback) {
+  this._connection.request(
+    (this._id === null) ? 'POST' : 'PUT',
+    this._database.name() + '/' + ((this._id === null) ? '' : this._id),
+    (function (error, response) {
+      if (response) {
+        this._id = response.id;
+        this._revision = response.rev;
+      }
+
+      callback(error, this);
+    }).bind(this),
+    body
+  );
+};
+
+
 exports.Document = Document;
