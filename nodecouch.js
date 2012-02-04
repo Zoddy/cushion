2e8142c480c5036aa7c74a4daec706d8ca2b6bf4
André Kussmann <zoddy@meecrom.de>
2012-02-04 09:37:23 +0100

    better request function with correct response headers and little bit commenting

--- nodecouch.js
+++ nodecouch.js
@@ -118,31 +118,40 @@ nodecouch.prototype._request = function(callback, response) {
  */
 nodecouch.prototype.request = function(properties) {
   var options = {
-    'host': this._options.host,
-    'port': this._options.port,
-    'method': (
-                typeof(properties.method) === 'string' &&
-                properties.method.match(this._methodMatch) !== null
-              ) ?
-              properties.method :
-              'GET',
-    'path': '/' + (properties.path || ''),
-    'auth': this._options.username + ':' + this._options.password,
-    'headers': properties.headers || null,
-  },
+        'host': this._options.host,
+        'port': this._options.port,
+        'method': (
+                    typeof(properties.method) === 'string' &&
+                    properties.method.match(this._methodMatch) !== null
+                  ) ?
+                  properties.method :
+                  'GET',
+        'path': '/' + (properties.path || ''),
+        'auth': this._options.username + ':' + this._options.password,
+        'headers': properties.headers || {},
+      },
+      request;
+
+  // make sure, that we get application/json response from couchdb
+  options.headers.Accept = options.headers.Accept || '*/*,application/json';
+
+  // set up request object
   request = http.request(
     options,
     this._request.bind(this, properties.callback)
   );
 
+  // define callback, if there is an error
   request.on('error', (function(error) {
     properties.callback(error, null);
   }).bind(this));
 
+  // adding optional body to the request
   if (typeof(properties.body) === 'object') {
     request.write(JSON.stringify(properties.body));
   }
 
+  // starting request
   request.end();
 };
 
