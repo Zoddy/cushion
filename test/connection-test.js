2f6a820040144569637da98b4adf1617fd149847
André Kussmann <zoddy@meecrom.de>
2012-02-18 23:42:45 +0100

    new and better tests and switching from vows to mocha

--- test/connection-test.js
+++ test/connection-test.js
@@ -1,41 +1,87 @@
-// change this to connect to your database
-var config = require('./config.js'),
-    assert = require('assert'),
-    vows = require('vows'),
-    nodecouch = require('../nodecouch.js'),
-    couch = new nodecouch.Connection(
-      config.host,
-      config.port,
-      config.username,
-      config.password
-    );
+/**
+ * Connection tests
+ *
+ * 1) check version
+ * 2) creating a database object
+ * 3) getting list of databases
+ */
 
-vows.describe('nodecouch connection').addBatch({
-  'when getting version': {
-    'topic': function() {
-      couch.version(this.callback);
-    },
+var assert = require('assert'),
+    mockup = function(properties) {
+               properties.callback(properties, null);
+             },
+    nodecouch = new (require('../nodecouch.js').Connection)(
+                  'localtest',
+                  '5984',
+                  'foo',
+                  'bar'
+                );
 
-    'we get a version string': function(error, response) {
-      assert.isString(response);
-    }
-  },
-  'when getting list of databases': {
-    'topic': function() {
-      couch.listDatabases(this.callback);
-    },
 
-    'we get an array of nodecouch database objects': function(error, response) {
-      assert.isArray(response);
-    }
-  },
-  'when creating database object': {
-    'topic': function() {
-      return couch.database(config.database);
-    },
+// overwrite original request function
+nodecouch.request = mockup;
 
-    'we get a database object with correct name': function(topic) {
-      assert.strictEqual(topic.name(), config.database);
-    }
-  }
-}).export(module);
+
+// starting tests
+describe('connection', function() {
+  describe('get version', function() {
+    it('should return a version string', function(done) {
+      nodecouch.version(function(properties) {
+        assert.strictEqual(
+          properties.method,
+          'GET',
+          'http method have to be GET'
+        );
+
+        assert.strictEqual(
+          properties.path,
+          '',
+          'path have to be an empty string'
+        );
+
+        assert.strictEqual(
+          typeof(properties.callback),
+          'function',
+          'callback have to be a function'
+        );
+
+        done();
+      });
+    });
+  });
+
+  describe('create a database object', function() {
+    it('should return a new database object', function() {
+      var database = nodecouch.database('foo');
+
+      assert.strictEqual(typeof(database), 'object', 'have to be an object');
+      assert.strictEqual(database._name, 'foo', 'db name have to be "foo"');
+    });
+  });
+
+  describe('get a list of databases', function() {
+    it('should return an array with database objects', function(done) {
+      nodecouch.listDatabases(function(properties) {
+        assert.strictEqual(
+          properties.method,
+          'GET',
+          'http method have to be GET'
+        );
+
+        assert.strictEqual(
+          properties.path,
+          '_all_dbs',
+          'path have to be "_all_dbs"'
+        );
+
+        assert.strictEqual(
+          typeof(properties.callback),
+          'function',
+          'callback have to be a function'
+        );
+
+        done();
+      });
+    });
+  });
+});
