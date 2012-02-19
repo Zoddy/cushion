365e9fd4085e09435da3de06141d893f12b2a8c6
André Kussmann <zoddy@meecrom.de>
2012-02-19 01:12:01 +0100

    better style for connection tests

--- test/connection-test.js
+++ test/connection-test.js
@@ -7,8 +7,8 @@
  */
 
 var assert = require('assert'),
-    mockup = function(properties) {
-               properties.callback(properties, null);
+    mockup = function(props) {
+               props.callback(props, null);
              },
     nodecouch = new (require('../nodecouch.js').Connection)(
                   'localtest',
@@ -26,21 +26,11 @@ nodecouch.request = mockup;
 describe('connection', function() {
   describe('get version', function() {
     it('should return a version string', function(done) {
-      nodecouch.version(function(properties) {
+      nodecouch.version(function(props) {
+        assert.strictEqual(props.method, 'GET', 'http method have to be GET');
+        assert.strictEqual(props.path, '', 'path have to be an empty string');
         assert.strictEqual(
-          properties.method,
-          'GET',
-          'http method have to be GET'
-        );
-
-        assert.strictEqual(
-          properties.path,
-          '',
-          'path have to be an empty string'
-        );
-
-        assert.strictEqual(
-          typeof(properties.callback),
+          typeof(props.callback),
           'function',
           'callback have to be a function'
         );
@@ -61,21 +51,15 @@ describe('connection', function() {
 
   describe('get a list of databases', function() {
     it('should return an array with database objects', function(done) {
-      nodecouch.listDatabases(function(properties) {
+      nodecouch.listDatabases(function(props) {
+        assert.strictEqual(props.method, 'GET', 'http method have to be GET');
         assert.strictEqual(
-          properties.method,
-          'GET',
-          'http method have to be GET'
-        );
-
-        assert.strictEqual(
-          properties.path,
+          props.path,
           '_all_dbs',
           'path have to be "_all_dbs"'
         );
-
         assert.strictEqual(
-          typeof(properties.callback),
+          typeof(props.callback),
           'function',
           'callback have to be a function'
         );
