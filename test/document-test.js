fd6cfef0e5405c867d2f8e3b7501d2126d7ed134
André Kussmann <zoddy@meecrom.de>
2012-02-26 00:07:34 +0100

    tests for document object

---
+++ test/document-test.js
@@ -0,0 +1,104 @@
+/**
+ * Document tests
+ *
+ * 1) create document with given id
+ * 2) create document without id
+ * 3) load document
+ * 4) copy document
+ * 5) save document
+ * 6) delete document
+ */
+
+var check = require('./check.js'),
+    mockup = function(properties) {
+               properties.callback(properties, null);
+             },
+    nodecouch = new (require('../nodecouch.js').Connection)(
+                  'localtest',
+                  '5984',
+                  'foo',
+                  'bar'
+                ),
+    database = nodecouch.database('foodb'),
+    docWithId = database.document('foodoc'),
+    docWithoutId = database.document();
+
+
+// overwrite original request function
+nodecouch.request = mockup;
+
+
+// starting tests
+describe('document', function() {
+  describe('create document with given id', function() {
+    it('should create the document', function(done) {
+      docWithId.create({'foo': 'bar'}, function(properties) {
+        check(properties, 'PUT', 'foodb/foodoc', {'foo': 'bar'});
+
+        done();
+      });
+    });
+  });
+
+  describe('create document without id', function() {
+    it('should create the document and returns generated id', function(done) {
+      docWithoutId.create({'foo': 'bar'}, function(properties) {
+        check(properties, 'POST', 'foodb', {'foo': 'bar'});
+
+        done();
+      });
+    });
+  });
+
+  describe('load document', function() {
+    it('should load the document body and revision', function(done) {
+      docWithId.load(function(properties) {
+        check(properties, 'GET', 'foodb/foodoc');
+
+        done();
+      });
+    });
+  });
+
+  describe('copy document', function() {
+    it('should copy the current document', function(done) {
+      docWithId.copy('copyDoc', function(properties) {
+        check(
+          properties,
+          'COPY',
+          'foodb/foodoc',
+          null,
+          {'Destination': 'copyDoc'}
+        );
+
+        done();
+      });
+    });
+  });
+
+  describe('save document', function() {
+    it('should validate the save', function(done) {
+      docWithId._revision = 'foorev';
+      docWithId.save({'foo': 'bar'}, function(properties) {
+        check(
+          properties,
+          'PUT',
+          'foodb/foodoc',
+          {'foo': 'bar', '_rev': 'foorev'}
+        );
+
+        done();
+      });
+    });
+  });
+
+  describe('delete document', function() {
+    it('should validate it', function(done) {
+      docWithId.destroy(function(properties) {
+        check(properties, 'DELETE', 'foodb/foodoc?rev=foorev');
+
+        done();
+      });
+    });
+  });
+});
