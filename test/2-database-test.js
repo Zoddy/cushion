d9756af409d15d634665defb692ae80656d4253d
André Kussmann <zoddy@zoddy.de>
2012-07-15 19:24:31 +0200

    add url checks for database

--- test/2-database-test.js
+++ test/2-database-test.js
@@ -15,30 +15,35 @@ var expect = require('chai').expect,
 exports.tests = [{
   'message': 'check for existing',
   'callpath': 'database.exists',
+  'url': ['GET', config.database],
   'callback': function(error, exists) {
     expect(exists).to.be.false;
   }
 }, {
   'message': 'create database',
   'callpath': 'database.create',
+  'url': ['PUT', config.database],
   'callback': function(error, created) {
     expect(created).to.be.true;
   }
 }, {
   'message': 'getting info',
   'callpath': 'database.info',
+  'url': ['GET', config.database],
   'callback': function(error, info) {
     expect(info).to.be.an('object').and.to.have.property('doc_count');
   }
 }, {
   'message': 'start compaction',
   'callpath': 'database.compact',
+  'url': ['POST', config.database + '/_compact'],
   'callback': function(error, started) {
     expect(started).to.be.true;
   }
 }, {
   'message': 'start cleanup',
   'callpath': 'database.cleanup',
+  'url': ['POST', config.database + '/_view_cleanup'],
   'callback': function(error, started) {
     expect(started).to.be.true;
   }
