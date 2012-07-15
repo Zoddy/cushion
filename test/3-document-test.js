392cf537fdd27a4b2e549bad0c260a071e630c0e
André Kussmann <zoddy@zoddy.de>
2012-07-15 19:24:46 +0200

    add url checks for document

--- test/3-document-test.js
+++ test/3-document-test.js
@@ -14,18 +14,33 @@
  * 10) delete document
  */
 
-var expect = require('chai').expect,
-    config = require('./config.js');
+var fs = require('fs'),
+    expect = require('chai').expect,
+    config = require('./config.js'),
+    docUrl = new RegExp(
+      '^' + config.database + '\\/' +
+      config.document +
+      '\\?rev=[0-9]{1,}-[0-9a-z]{1,}$'
+    ),
+    attachmentUrl = new RegExp(
+      '^' + config.database +
+      '\\/' + config.document +
+      '\\/(Makefile|' + config.attachment + ')' +
+      '\\?rev=[0-9]{1,}-[0-9a-z]{1,}$'
+    ),
+    attachment = fs.readFileSync(__dirname + '/../Makefile', 'utf8');
 
 exports.tests = [{
   'message': 'save document',
   'callpath': 'document.save',
+  'url': ['PUT', config.database + '/' + config.document, {}],
   'callback': function(error, document) {
     expect(document).to.be.an('object').and.to.have.property('_id');
   },
 }, {
   'message': 'load document',
   'callpath': 'document.load',
+  'url': ['GET', docUrl],
   'callback': function(error, document) {
     expect(document).to.be.an('object').and.to.have.property('_id');
     expect(document.id()).to.equal(config.document);
@@ -34,6 +49,12 @@ exports.tests = [{
   'message': 'copy document',
   'callpath': 'document.copy',
   'arguments': [config.document + '_copy'],
+  'url': [
+    'COPY',
+    docUrl,
+    undefined,
+    {'Destination': config.document + '_copy'}
+  ],
   'callback': function(error, source, target) {
     expect(source).to.be.an('object').and.to.have.property('_id');
     expect(source.id()).to.be.equal(config.document);
@@ -43,6 +64,7 @@ exports.tests = [{
 }, {
   'message': 'info about the document',
   'callpath': 'document.info',
+  'url': ['HEAD', config.database + '/' + config.document],
   'callback': function(error, info) {
     expect(info).to.be.an('object')
     expect(info).to.have.property('revision')
@@ -52,6 +74,15 @@ exports.tests = [{
   'message': 'save attachment',
   'callpath': 'document.saveAttachment',
   'arguments': [__dirname + '/../Makefile', 'text/plain'],
+  'url': [
+    'PUT',
+    attachmentUrl,
+    attachment,
+    {
+      'Content-Length': attachment.length,
+      'Content-Type': 'text/plain'
+    }
+  ],
   'callback': function(error, confirmed) {
     expect(confirmed).to.be.true;
   }
@@ -59,6 +90,15 @@ exports.tests = [{
   'message': 'save attachment with custom name',
   'callpath': 'document.saveAttachment',
   'arguments': [__dirname + '/../Makefile', 'text/plain', config.attachment],
+  'url': [
+    'PUT',
+    attachmentUrl,
+    attachment,
+    {
+      'Content-Length': attachment.length,
+      'Content-Type': 'text/plain'
+    }
+  ],
   'callback': function(error, confirmed) {
     expect(confirmed).to.be.true;
   }
@@ -66,6 +106,10 @@ exports.tests = [{
   'message': 'load attachment',
   'callpath': 'document.getAttachment',
   'arguments': [config.attachment],
+  'url': [
+    'GET',
+    attachmentUrl
+  ],
   'callback': function(error, attachment) {
     expect(attachment).to.be.a('string');
   }
@@ -73,19 +117,22 @@ exports.tests = [{
   'message': 'delete attachment',
   'callpath': 'document.deleteAttachment',
   'arguments': ['Makefile'],
+  'url': ['DELETE', attachmentUrl],
   'callback': function(error, deleted) {
     expect(deleted).to.be.true;
   }
 }, {
-  'message': 'delete document with custom name',
+  'message': 'delete attachment with custom name',
   'callpath': 'document.deleteAttachment',
   'arguments': [config.attachment],
+  'url': ['DELETE', attachmentUrl],
   'callback': function(error, deleted) {
     expect(deleted).to.be.true;
   }
 }, {
   'message': 'delete document',
   'callpath': 'document.destroy',
+  'url': ['DELETE', docUrl],
   'callback': function(error, document) {
     expect(document).to.be.an('object').and.to.have.property('_id');
   }
