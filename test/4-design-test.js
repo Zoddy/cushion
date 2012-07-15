57642fe7d1b56a2b7dff0031230970e4c9089d62
André Kussmann <zoddy@zoddy.de>
2012-07-15 20:20:30 +0200

    add url checks for design api

--- test/4-design-test.js
+++ test/4-design-test.js
@@ -6,17 +6,41 @@
  * 2) starting compaction
  */
 
-var expect = require('chai').expect;
+var expect = require('chai').expect,
+    config = require('./config.js');
 
 exports.tests = [{
   'message': 'save design document',
   'callpath': 'design.save',
+  'url': [
+    'PUT',
+    config.database + '/_design/' + config.design,
+    {
+      lists: {
+        test_list: 'function(head, request) {' +
+          'var row;start({"headers": {"Content-Type": "text/plain"}});' +
+          'while(row = getRow()) {send(row.value._id);}' +
+        '}'
+      },
+      shows: {
+        test_show: 'function(document, request) {' +
+          'return {' +
+            'body: "Hello " + JSON.stringify(request.query) + " " + request.id' +
+          '};' +
+        '}'
+      },
+      views: {
+        test_view: { map: 'function(doc) {emit(doc._id, doc);}' }
+      }
+    }
+  ],
   'callback': function(error, document) {
     expect(document).to.be.an('object').and.to.respondTo('view');
   }
 }, {
   'message': 'starting compaction',
   'callpath': 'design.compact',
+  'url': ['POST', config.database + '/_compact/' + config.design],
   'callback': function(error, started) {
     expect(started).to.be.true;
   }
